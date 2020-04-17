7422221ef5d1788250c0f9ce3830b215b0e90ac8
Roman <roman.svitelskyi@gmail.com>
2020-04-17 13:42:16 +0300

    changes

--- api/routers.js
+++ api/routers.js
@@ -3,22 +3,22 @@ const CocntactController = require("./controlers");
 
 const router = Router();
 
-router.get("/", CocntactController.listContacts());
+router.get("/", CocntactController.listContacts);
 router.get(
   "/:contactId",
-  CocntactController.validateId(),
-  CocntactController.getContactById()
+  CocntactController.validateId,
+  CocntactController.getContactById
 );
-router.post("", CocntactController.addContact());
+router.post("", CocntactController.addContact);
 router.delete(
   "/:contactId",
-  CocntactController.validateId(),
-  CocntactController.removeContact()
+  CocntactController.validateId,
+  CocntactController.removeContact
 );
 router.patch(
   "/:contactId",
-  CocntactController.validateId(),
-  CocntactController.updateContact()
+  CocntactController.validateId,
+  CocntactController.updateContact
 );
 
 module.exports = router;
